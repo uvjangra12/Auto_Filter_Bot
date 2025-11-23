640cfd2e476515e92704140a34ee8de14173250f
uvjangra12 <uvjangra3@gmail.com>
2025-11-23 19:42:04 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -224,7 +224,7 @@ Nᴀᴍᴇ : {}
 
 Mᴇꜱꜱᴀɢᴇ : <b>{}</b>"""
     
-    CAPTION = """<b><a href="https://t.me/dreamxbotz">{file_name}</a></b>\n\n<b>⚜️ Powered By : <a href="https://t.me/dreamxbotz">[ ᴅʀᴇᴀᴍxʙᴏᴛᴢ ]</a></b>"""
+    CAPTION = "<a href=https://t.me/hmmmmw876>@hmmmw876 {file_name} </a>"
 
     
     MOVIE_UPDATE_NOTIFY_TXT = """
